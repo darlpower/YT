40e7bc8af39c6e264f387561cd25ebe75ab2328c
HsinHungLin <t77798026@ntut.org.tw>
2020-12-22 10:45:50 +0800

    Successfully read captions into data structure

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,9 +1,11 @@
+from yt_concate.pipeline.steps.preflight import Preflights
 from yt_concate.pipeline.steps.get_video_list import GetVideoList
 from yt_concate.pipeline.steps.download_caption import DownloadCaptions
+from yt_concate.pipeline.steps.read_caption import ReadCaption
+from yt_concate.pipeline.steps.postflight import Postflights
 from yt_concate.pipeline.pipeline import Pipeline
 from yt_concate.utils import Utils
-from yt_concate.pipeline.steps.preflight import Preflights
-from yt_concate.pipeline.steps.postflight import Postflights
+
 
 #CHANNEL_ID = "UCKSVUHI9rbbkXhvAXK-2uxA"
 CHANNEL_ID ='UCIldsycnma5sHR1VRP38vhg'
@@ -16,6 +18,7 @@ def main():
         Preflights(),
         GetVideoList(),
         DownloadCaptions(),
+        ReadCaption(),
         Postflights(),
     ]
 
